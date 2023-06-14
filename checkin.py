04a95c6bef3261ccd97e195e13b870cabf69783b
xuyanke7 <512606466@qq.com>
2023-06-14 16:41:25 +0800

    Upgate flow,add log

--- checkin.py
+++ checkin.py
@@ -1,13 +1,18 @@
 import requests
 import json
+import logging
 import os
 
-# off不开启(默认)，on开启cookie失效通知和签到成功通知
-sever = os.environ["SERVE"]
-# sckey,不开启则不用填
-sckey = os.environ["SCKEY"]
+sever = "on"
+cookie = "koa:sess=eyJ1c2VySWQiOjIwNDczNywiX2V4cGlyZSI6MTcwNDA4NTY2NzQ2MSwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=hTBINafo3XYOBQwp2TNfQ0OPd2A; _gid=GA1.2.169040157.1686730282; _gat_gtag_UA_104464600_2=1; _ga=GA1.1.1206574097.1678165680; _ga_CZFVKMNT9J=GS1.1.1686730281.3.1.1686730497.0.0.0"
 
-cookie = os.environ["COOKIE"]
+
+logging.basicConfig(
+    level=logging.INFO, 
+    filename="logging.log", 
+    filemode="a",
+    format='%(asctime)s - %(levelname)s - %(message)s',
+)
 
 
 def start():
@@ -18,24 +23,23 @@ def start():
     referer = "https://glados.rocks/console/checkin"
     useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
     payload = {
-        'token': 'glados_network'
+        'token': 'glados.network'
     }
     checkin = requests.post(url, headers={'cookie': cookie, 'referer': referer, 'origin': origin,
                             'user-agent': useragent, 'content-type': 'application/json;charset=UTF-8'}, data=json.dumps(payload))
     state = requests.get(url2, headers={
                          'cookie': cookie, 'referer': referer, 'origin': origin, 'user-agent': useragent})
-   # print(res)
 
     if 'message' in checkin.text:
         mess = checkin.json()['message']
         time = state.json()['data']['leftDays']
         time = time.split('.')[0]
-        # print(time)
         if sever == 'on':
-            requests.get('https://sc.ftqq.com/' + sckey +
-                         '.send?text='+mess+'，you have '+time+' days left')
+            # print(mess)
+            logging.info(mess)
     else:
-        requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
+        # 
+        logging.error("Cookie Expired")
 
 
 def main_handler(event, context):
