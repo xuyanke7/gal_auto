240026f8a4cfbb729cba44265d1a1d09fa02e238
xuyifan <512606466@qq.com>
2024-03-08 20:57:33 +0800

    update

--- checkin.py
+++ checkin.py
@@ -6,6 +6,8 @@ import os
 sever = "on"
 cookie = os.getenv("COOKIE")
 sckey = os.getenv("SCKEY")
+sckey = "SCT213504TB5Ot5vjoVcHIJEIA5MlZqWwl"
+cookie = "koa:sess=eyJ1c2VySWQiOjIwNDczNywiX2V4cGlyZSI6MTczMTE2MzM2NDc5NSwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=aVwocC76tFq0ooRiYKbUsBkySEc; __stripe_mid=505da4b2-082b-478e-9bb9-98ba19a065a118077d; __stripe_sid=04c07890-446c-4e15-b44a-67bbaca38a80260be8; _gid=GA1.2.700082131.1709102231; _gat_gtag_UA_104464600_2=1; _ga=GA1.1.1009244622.1703344732; _ga_CZFVKMNT9J=GS1.1.1709102231.6.1.1709102267.0.0.0"
 
 logging.basicConfig(
     level=logging.INFO,
@@ -21,9 +23,9 @@ def start():
     url2 = "https://glados.rocks/api/user/status"
     origin = "https://glados.rocks"
     referer = "https://glados.rocks/console/checkin"
-    useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
+    useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
     payload = {
-        'token': 'glados.network'
+        'token': 'glados.one'
     }
     checkin = requests.post(url, headers={'cookie': cookie, 'referer': referer, 'origin': origin,
                             'user-agent': useragent, 'content-type': 'application/json;charset=UTF-8'}, data=json.dumps(payload))
@@ -35,13 +37,13 @@ def start():
         time = state.json()['data']['leftDays']
         time = time.split('.')[0]
         if sever == 'on':
-            print(mess)
+            print(mess + ', you have '+time+' days left')
             logging.info(mess)
-            requests.get('https://sc.ftqq.com/' + sckey +
+            requests.post('https://sc.ftqq.com/' + sckey +
                          '.send?text=' + mess + ', you have '+time+' days left')
     else:
         #
-        requests.get('https://sc.ftqq.com/' + sckey +
+        requests.post('https://sc.ftqq.com/' + sckey +
                      '.send?text=Cookie Expired')
         logging.error("Cookie Expired")
 
